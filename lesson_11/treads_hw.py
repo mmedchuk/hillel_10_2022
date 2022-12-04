a0b3c10863acef70fed975c6851d898654072aaa
mmedchuk <m.medchuk@gmail.com>
2022-12-04 22:56:49 +0200

    changed func get_primes and print result as one list

--- lesson_11/treads_hw.py
+++ lesson_11/treads_hw.py
@@ -2,6 +2,8 @@ from math import ceil
 from threading import Thread
 from typing import Generator
 
+PRIMES_LIST: list = []
+
 
 def get_ranges(start: int, end: int, amount: int) -> Generator:
     part_len: int = ceil(len(range(start, end)) / amount)
@@ -23,18 +25,26 @@ def get_primes_list(start: int, end: int) -> list[int]:
                 break
         if prime:
             results.append(number)
-    print(results)
+    for i in results:
+        PRIMES_LIST.append(i)
 
 
 def get_primes(start: int, end: int, amount_of_threads: int) -> list[int]:
 
     ranges = get_ranges(start, end, amount_of_threads)
 
+    threads = []
+
     for i in range(amount_of_threads):
         start_range, end_range = next(ranges)
         thread = Thread(target=get_primes_list, args=(start_range, end_range))
         thread.start()
+        threads.append(thread)
+
+    for thread in threads:
         thread.join()
+
+    print(PRIMES_LIST)
 
 
 get_primes(10, 1000, 10)
